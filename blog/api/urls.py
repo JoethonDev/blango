dcae8b8f53dfd7952895e4c141d5d15ef03f41e2
George Zakhary <elprincedoca@gmail.com>
2024-10-14 15:39:38 +0000

    Finish viewsets and routers

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path, include, re_path
 from rest_framework.urlpatterns import format_suffix_patterns
-from blog.api.views import PostDetail, PostList, UserDetail
+from rest_framework.routers import DefaultRouter
+from blog.api.views import PostViewSet, UserDetail, TagViewSet
 from rest_framework.authtoken import views
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
 
+# Making Router
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
+
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token)
@@ -38,4 +42,5 @@ urlpatterns += [
       schema_view.with_ui("swagger", cache_timeout=0),
       name="schema-swagger-ui",
   ),
+  path("", include(router.urls))
 ]
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,24 +1,50 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import action
+from rest_framework.response import Response
 # from rest_framework.permissions import 
-from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer
-from blog.models import Post
+from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer, TagSerializer
+from blog.models import Post, Tag
 from blango_auth.models import User
 
+# PostViewSet Subsutite below classes
+# class PostList(generics.ListCreateAPIView):
+#     authentication_classes = [SessionAuthentication]
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
 
-class PostList(generics.ListCreateAPIView):
-    authentication_classes = [SessionAuthentication]
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+
+# class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostDetailSerializer
 
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+class PostViewSet(viewsets.ModelViewSet):
+  queryset = Post.objects.all()
 
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+  def get_serializer_class(self):
+    if self.action in ("list", "create"):
+      return PostSerializer
+    return PostDetailSerializer
 
 
 class UserDetail(generics.RetrieveAPIView):
     lookup_field = "email"
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class TagViewSet(viewsets.ModelViewSet):
+  queryset = Tag.objects.all()
+  serializer_class = TagSerializer
+
+  @action(methods=['get'], detail=True, name="Posts with the Tag")
+  def posts(self, request, pk=None):
+    tag = self.get_object()
+    post_serializer = PostSerializer(
+      tag.posts,
+      context= {
+        "request" : request
+      },
+      many=True
+    )
+    return Response(post_serializer.data)
